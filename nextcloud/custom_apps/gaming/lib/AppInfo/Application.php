d068ac58b285b394710940cd680df099c58c4f12
Jorge <jorgemartinezpizarro@gmail.com>
2026-07-05 18:06:45 +0200

    update nc module

--- nextcloud/custom_apps/gaming/lib/AppInfo/Application.php
+++ nextcloud/custom_apps/gaming/lib/AppInfo/Application.php
@@ -2,23 +2,10 @@
 
 namespace OCA\Gaming\AppInfo;
 
-use OCA\Gaming\Activity\ScoreProvider;
-use OCA\Gaming\Activity\ScoreSetting;
 use OCP\AppFramework\App;
-use OCP\AppFramework\Bootstrap\IBootContext;
-use OCP\AppFramework\Bootstrap\IBootstrap;
-use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application extends App implements IBootstrap {
+class Application extends App {
     public function __construct() {
         parent::__construct('gaming');
-    }
-
-    public function register(IRegistrationContext $context): void {
-        $context->registerActivitySetting(ScoreSetting::class);
-        $context->registerActivityProvider(ScoreProvider::class);
-    }
-
-    public function boot(IBootContext $context): void {
     }
 }
